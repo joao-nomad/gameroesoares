055cdd0c494f826d135066cf23945070f077899b
Joao Vitor <jvcadastro@gmail.com>
2017-01-02 19:43:31 -0200

    Almost ready menu

--- js/script.js
+++ js/script.js
@@ -1,11 +1,24 @@
 $("#menuBars").click(function() {
-	//hasClass
-  $("#responsiveBar").slideDown("slow");
+	if ($("#menuBars").hasClass("barsIco") == true){
+		$("#menuBars").removeClass("barsIco");
+		$("#menuBars").addClass("closeIco");
+
+		$("#responsiveBar").slideDown("slow");
+	}
+
+	else if ($("#menuBars").hasClass("closeIco") == true){
+		$("#menuBars").removeClass("closeIco");
+		$("#menuBars").addClass("barsIco");
+
+		$("#responsiveBar").slideUp("slow");
+	}
 });
 
 
 $(window).resize(function(){
 	if ($(window).width() > 1230){
 		$("#responsiveBar").css("display", "none");
+		$("#menuBars").removeClass("closeIco");
+		$("#menuBars").addClass("barsIco");
 	}
 });
