3339978db51aff355f4fe0d81c7de3551f7c1a4e
Joao Vitor <jvcadastro@gmail.com>
2017-01-04 22:43:41 -0200

    teste

--- js/script.js
+++ js/script.js
@@ -22,3 +22,22 @@ $(window).resize(function(){
 		$("#menuBars").addClass("barsIco");
 	}
 });
+
+function explode(){
+  if ($("#bg1").css("display") == "block"){
+  	$("#bg1").fadeOut("fast");
+  	$("#bg2").fadeIn("fast");
+  }
+  else if ($("#bg2").css("display") == "block"){
+  	$("#bg2").fadeOut("fast");
+  	$("#bg3").fadeIn("fast");
+  }
+  else if ($("#bg3").css("display") == "block"){
+  	$("#bg3").fadeOut("fast");
+  	$("#bg1").fadeIn("fast");
+  } 
+
+  setTimeout(explode, 4000);
+}
+
+setTimeout(explode, 4000);
