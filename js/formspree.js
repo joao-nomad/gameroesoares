cf198fde213b2612e949b4abc189fedf4d1f4049
João Vitor Lessa <jvcadastro@gmail.com>
2017-01-11 11:52:43 -0200

    form

--- js/formspree.js
+++ js/formspree.js
@@ -2,20 +2,20 @@ var $contactForm = $('#contact-form');
 	$contactForm.submit(function(e) {
 	e.preventDefault();
 	$.ajax({
-		url: 'https://formspree.io/joaovitorlessa@gmail.com',
+		url: 'https://formspree.io/contato@gameroesoares.com.br',
 		method: 'POST',
 		data: $(this).serialize(),
 		dataType: 'json',
 		beforeSend: function() {
-			$contactForm.append('<div class="alert alert--loading">Enviando Mensagem…</div>');
+			$contactForm.append('<div class="alert alert--loading" style="color: white;">Enviando Mensagem…</div>');
 		},
 		success: function(data) {
 			$contactForm.find('.alert--loading').hide();
-			$contactForm.append('<div class="alert alert--success">Mensagem enviada!</div>');
+			$contactForm.append('<div class="alert alert--success style="color: white;"">Mensagem enviada!</div>');
 		},
 		error: function(err) {
 			$contactForm.find('.alert--loading').hide();
-			$contactForm.append('<div class="alert alert--error">Ops, erro no servidor de hospedagem.</div>');
+			$contactForm.append('<div class="alert alert--error" style="color: white;">Ops, erro no servidor de hospedagem.</div>');
 		}
 	});
 });
